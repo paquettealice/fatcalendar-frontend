518b41c754d8f9dee4030a4e35699a9e4ee9e17d
paquettepy <paquette.py@gmail.com>
2018-12-13 17:33:49 -0500

    first

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { StaticModule } from './static/static.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        StaticModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +22,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fatcalendar-frontend'`, () => {
+  it(`should have as title 'Fat Calendar'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('fatcalendar-frontend');
+    expect(app.title).toEqual('Fat Calendar');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should have a router-outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to fatcalendar-frontend!');
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
---
+++ src/app/static/static.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { WebsiteHeaderComponent } from './website-header/website-header.component';
+
+@NgModule({
+  imports: [
+    
+  ],
+  declarations: [
+    WebsiteHeaderComponent
+  ],
+  exports: [
+    WebsiteHeaderComponent
+  ]
+})
+export class StaticModule {
+  constructor() { }
+}
