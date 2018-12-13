a2c7f1c8888fb32ea9c255af3c578cf106ef6e5a
paquettepy <paquettepy@gmail.com>
2018-12-13 18:55:27 -0500

    header and footer

--- src/app/static/static.module.ts
+++ src/app/static/static.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { WebsiteHeaderComponent } from './website-header/website-header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { WebsiteFooterComponent } from './website-footer/website-footer.component';
 
 @NgModule({
   imports: [
-    
+    FlexLayoutModule
   ],
   declarations: [
-    WebsiteHeaderComponent
+    WebsiteHeaderComponent,
+    WebsiteFooterComponent
   ],
   exports: [
-    WebsiteHeaderComponent
+    WebsiteHeaderComponent,
+    WebsiteFooterComponent
   ]
 })
 export class StaticModule {
---
+++ src/app/static/website-footer/website-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-website-footer',
+  templateUrl: './website-footer.component.html',
+  styleUrls: ['./website-footer.component.scss']
+})
+export class WebsiteFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
